fc94b8dad48aa43acbd85585f2d2d311c5b94e9e
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-06-30 09:32:29 +0530

    LeetCode 01

--- LeetCode/Q1.java
+++ LeetCode/Q1.java
@@ -1,14 +1,13 @@
 // LeetCode 1480 - Running Sum of 1d Array
 import java.util.*;
 
-public class LC_01 {
-    public static int[] runningSum(int[] nums) {
+public class Q1 {
+    public static int[] runningSum(int nums[]) {
         for(int i=1 ; i<nums.length ; i++) {
            nums[i] = nums[i] + nums[i-1];
         }
         return nums;
     }
-}
 
 public static void main(String args[]) {
     int nums[] = {1, 2, 3, 4};
@@ -16,6 +15,7 @@ public static void main(String args[]) {
     int result[] = runningSum(nums);
 
     for(int i=0 ; i<result.length ; i++) {
-        System.out.println(result[i] + " ");
+        System.out.print(result[i] + " ");
     }
+}
 }
