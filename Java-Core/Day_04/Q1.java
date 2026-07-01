efe68cd5567f7642cc205cb0090a3574d60aef48
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-07-01 08:44:24 +0530

    Day 04 - Pairs

--- Java-Core/Day_04/Q1.java
+++ Java-Core/Day_04/Q1.java
@@ -4,13 +4,17 @@ import java.util.*;
 
 public class Q1 {
     public static void pairs(int numbers[]) {
+        int totalPairs = 0;
+
         for(int i=0 ; i<numbers.length ; i++) {
             int curr = numbers[i];
             for(int j=i+1 ; j<numbers.length ; j++) {
                 System.out.print("( " + curr + "," + numbers[j] + " )");
+                totalPairs++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs = " + totalPairs);
     }
     public static void main(String args[]) {
         int numbers[] = {2, 4, 6, 8, 10};
