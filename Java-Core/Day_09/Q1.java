923a6fcf800ccdc6507963d71896502adc671692
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-07-06 10:46:23 +0530

    Day 09 - Bubble Sort

---
+++ Java-Core/Day_09/Q1.java
@@ -0,0 +1,32 @@
+// Day 09 - Basic Sorting Algorithms - Bubble Sort
+// Date - 06/07/2026
+
+import java.util.*;
+
+public class Q1 {
+
+    public static void bubbleSort(int arr[]) {
+        for(int i=0 ; i<arr.length ; i++) {
+            for(int j=0 ; j<arr.length-1-i ; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for(int i=0 ; i<arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        bubbleSort(arr);
+        print(arr);
+    }
+}
