e52631537debfbdea1bec472b40af6633d8d9a56
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-07-07 11:47:51 +0530

    Day 10 - Insertion Sort

---
+++ Java-Core/Day_10/Q1.java
@@ -0,0 +1,30 @@
+// Day 10 - Insertion Sort
+// Date - 07/07/2026
+import java.util.*;
+
+public class Q1 {
+    public static void insertionSort(int arr[]) {
+        for(int i=1 ; i<arr.length ; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void print(int arr[]) {
+        for(int i=0 ; i<arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        insertionSort(arr);
+        print(arr);
+    }
+}
