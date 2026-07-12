a4e08f2d463a88065cb3a83f30d3d6156837918b
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-07-12 11:45:05 +0530

    Day 15 update

--- Java-Core/Day_14/Q1.java
+++ Java-Core/Day_14/Q1.java
@@ -1,12 +1,22 @@
-// Day 14 - 2D Arrays - Spiral Matrix
-// Date - 12/07/2026
+// Day 14 - 2D Arrays
+// Date- 11/07/2026
 import java.util.*;
 
-public class Q1 {
+public class Q5 {
+    public static boolean search(int matrix[][], int key) {
+        for(int i=0 ; i<matrix.length ; i++) {
+            for(int j=0 ; j<matrix[0].length ; j++) {
+                if(matrix[i][j] == key) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
-        int matrix[][] = {{1, 2, 3, 4},
-                            {5, 6, 7, 8},
-                            {9, 10, 11, 12},
-                            {13, 14, 15, 16}};
+        int matrix[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int key = 8;
+        System.out.println(search(matrix, key));
     }
 }
---
+++ Java-Core/Day_15/Q1.java
@@ -0,0 +1,12 @@
+// Day 15 - 2D Arrays - Spiral Matrix
+// Date - 12/07/2026
+import java.util.*;
+
+public class Q1 {
+    public static void main(String args[]) {
+        int matrix[][] = {{1, 2, 3, 4},
+                            {5, 6, 7, 8},
+                            {9, 10, 11, 12},
+                            {13, 14, 15, 16}};
+    }
+}
