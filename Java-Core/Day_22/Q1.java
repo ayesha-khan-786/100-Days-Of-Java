6f0d07a689870b7fcf333e3eebdb76ead39e35f0
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-07-21 09:55:39 +0530

    Max Water

---
+++ Java-Core/Day_22/Q1.java
@@ -0,0 +1,29 @@
+// Day 22 - Container with Most Water (#11)
+import java.util.*;
+
+public class Q1 {
+
+    public static int maxArea(int height[]) {
+        int left = 0;
+        int right = height.length-1;
+        int maxWater = 0;
+
+        while(left < right) {
+            int water = Math.min(height[left], height[right]) * (right - left);
+            maxWater = Math.max(maxWater, water);
+
+            if(height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+
+    public static void main(String args[]) {
+        int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+}
